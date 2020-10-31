a70e2a6632cffda9cdab5603cbbd8c837001bef4
Jasper <jaspervanderwaa@gmail.com>
2020-10-31 18:28:32 +0100

    Finished help, added !whoplays, added join event, added error handline, added !wazzup

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 # bot.py
 
-# TODO # - !help
-# TODO # - !whoplays
 # TODO # - !suggest
-# TODO # - message on errors
+# TODO # - welcome
 
 import functools
 import os
@@ -11,10 +9,15 @@ import random
 import math
 import re
 import string
+import sys
+import time
+
 import pandas as pd
 import sqlite3
+
+from discord.utils import get
 from matplotlib import pyplot as plt
-from discord import Activity, ActivityType, File, Intents
+from discord import Activity, ActivityType, File, Intents, DiscordException
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -47,6 +50,25 @@ discord_blue = "#7087E4"
 discord_gray = "#36393F"
 discord_white = "#FFFFFB"
 
+# Emojis
+em_wave = ":wave:"
+em_smile = ":smiley_cat:"
+em_sad = ":crying_cat_face:"
+
+# ASCII emojis
+em_throw_table = "(╯°□°）╯︵ ┻━┻"
+em_put_table = "┬──┬◡ﾉ(° -°ﾉ)"
+em_play_game = "{\\\__/}\n" \
+               "(●_●)\n" \
+               "( >🎮 Play game?"
+em_panda_face = "(◉ܫ◉)"
+em_game_on = "(ง •̀_•́)ง"
+em_dance = "(~‾▿‾)~"
+rnd_emotes = [em_panda_face, em_game_on, em_dance]
+
+# A place to store any exceptions that occurred for a certain server
+exceptions = {}
+
 
 #######################
 # Some helper methods #
@@ -61,7 +83,7 @@ def style(txt):
     return f"```diff\n{txt}\n```"
 
 
-# Status decorator
+# Status decorator and checks
 def status_update(func):
     @functools.wraps(func)  # Important to preserve name because `command` uses it
     async def wrapper(*args, **kwargs):
@@ -69,6 +91,11 @@ def status_update(func):
             ctx = kwargs["ctx"]
         else:
             ctx = args[0]
+
+        # Check if the guild/server is allowed
+        guild_name = ctx.guild.name
+        if guild_name not in ALLOWED_GUILDS:
+            raise ValueError(f"The guild {guild_name} is not allowed to run this bot.")
 
         # Show that the bot is busy
         with ctx.channel.typing():
@@ -145,8 +172,6 @@ async def get_games(ctx, user_name, n_games):
 
     # Get the games data
     guild_name = ctx.guild.name
-    if guild_name not in ALLOWED_GUILDS:
-        raise ValueError(f"The guild {guild_name} is not allowed to run this bot.")
     guild_df = pd.read_sql_query(f"SELECT * from {guild_sql_table(guild_name)}", sql_connection)
 
     # Get the listed games of the requested user if given, otherwise get all listed games
@@ -162,14 +187,14 @@ async def get_games(ctx, user_name, n_games):
             # If no user has this user name, send a message and return
             if len(user_id) == 0:
                 await channel.send(style(f"It seems I cannot find {user_name} in this server. Did you spell it "
-                                   f"correctly?"))
+                                         f"correctly?"))
                 return None, None, None
 
             else:
                 if len(user_id) > 1:
                     # Multiple users have this name, which the bot handles by sending a message and pick the first
                     await channel.send(style(f"It seems I found more people with the name {user_name} in this server. "
-                                       f"Sadly, I don't know how to handle this so I just pick the first one."))
+                                             f"Sadly, I don't know how to handle this so I just pick the first one."))
 
                 # We pick the first user with this user name
                 user_id = user_id[0]
@@ -190,16 +215,19 @@ async def get_games(ctx, user_name, n_games):
     # If we have no listed games for that user or server
     if len(listed_games) == 0:
         if user_name is None:
-            await channel.send(style(f"It seems nobody in this server registered any games yet. Be the first! You can do "
-                               f"so with !add, for example '!add pubg, minecraft'."))
+            await channel.send(
+                style(f"It seems nobody in this server registered any games yet. Be the first! You can do "
+                      f"so with !add, for example '!add pubg, minecraft'."))
             return None, None, None
         elif user_name == "me":
-            await channel.send(style(f"It seems you did not register any games yet. You can do so with !add, for example "
-                               f"'!add pubg, minecraft'."))
+            await channel.send(
+                style(f"It seems you did not register any games yet. You can do so with !add, for example "
+                      f"'!add pubg, minecraft'."))
             return None, None, None
         else:
-            await channel.send(style(f"It seems that {user_name} did not yet register any games with me. {user_name} can "
-                               f"do so for themself with !add, for example '!add pubg, minecraft'."))
+            await channel.send(
+                style(f"It seems that {user_name} did not yet register any games with me. {user_name} can "
+                      f"do so for themself with !add, for example '!add pubg, minecraft'."))
             return None, None, None
 
     # Get the histogram of how popular these games are in the server
@@ -246,6 +274,49 @@ async def on_ready():
     else:
         print(f'{disco.user.name} has connected to Discord and created a new database!')
 
+
+# When an error/exception occurs
+@disco.event
+async def on_command_error(ctx, error):
+    # Still print it to the console
+    print(error, file=sys.stderr)
+
+    # Get the server on which the erroneous event occurred
+    guild_name = guild_sql_table(ctx.message.guild.name)
+
+    # Check if it was a unknown command error
+    if "is not found" in str(error):
+        cmd = ctx.message.content
+        mssg = f"I don't know the command {cmd} {em_sad}\nYou can type !help if you need some help."
+        await ctx.message.channel.send(mssg)
+
+    # If an exception is found, we prep the exceptions storage for this server if need be
+    if guild_name not in exceptions.keys():
+        exceptions[guild_name] = []
+
+    # We create our 'exception data' dict
+    exc = {"error": error, "context": ctx}
+
+    # If an exception occurred we store it
+    exceptions[guild_name].append(exc)
+
+
+# When a new member joins
+@disco.event
+async def on_member_join(member):
+    # Get the general channel of the server
+    channel = get(member.guild.channels, name="general")
+
+    # Welcome the new member
+    await channel.send(f"Welcome {member.name}! {em_wave}")
+    time.sleep(0.2)
+    await channel.send(f"I am Disco, and I track what games everyone plays. This way it becomes a lot "
+                       f"easier to see what is popular and who has the same games as you.")
+    time.sleep(0.2)
+    await channel.send("To add games you can use the command `!add`. For example; `!add pubg, Minecraft`. Which adds "
+                       "the games PUBG and Minecraft to your profile. To view or list registered games use `!view` or "
+                       "`!list` respectively. To view who plays a certain game use `!whoplays`. See `!help` for a "
+                       "complete explanation of everything I can do.")
 
 #####################
 # Listen to commands
@@ -318,12 +389,19 @@ async def list_games(ctx, user_name=None):
 @disco.command("remove")
 @status_update
 async def remove_games(ctx, *, game_list=None):
+    """ all|<game>[, <game>, ...]  Removes one or more games you have registered.
+
+    This can be used to remove all games you have registered (!remove all) or just a few (!remove <game_1>, <game_2>,
+    ...). On completion it provides an !add command to redo the removal if need be. Multiple games need to be
+    comma-separated.
+    """
+
     # Get all of the user's games
     _, hist, user_id = await get_games(ctx, user_name="me", n_games=854775807)
     registered_games = list(hist.index.values)
 
     # If the game list is None, we remove all games for that user
-    if game_list is None:
+    if game_list == "all":
         games = registered_games
 
     # Else, we parse the games list (separated by comma's if there are more)
@@ -388,13 +466,20 @@ async def remove_games(ctx, *, game_list=None):
 @disco.command("add")
 @status_update
 async def add_games(ctx, *, game_list=None):
+    """ <game>[, <game>, ...] Add one or more games.
+
+    Adds or registers one (!add <game>) or more games (!add <game>, <game>, ...). Multiple games need to be
+    comma-separated.
+    """
+
     # Get the channel in which the command was used
     channel = ctx.message.channel
 
     # If no games were provided send a help message
     if game_list is None:
-        await channel.send(style("If you want to add some games to your profile, you should tell me which. For example; "
-                           "'!add pubg, World of Warcraft, Minecraft'."))
+        await channel.send(
+            style("If you want to add some games to your profile, you should tell me which. For example; "
+                  "'!add pubg, World of Warcraft, Minecraft'."))
         return
 
     # Get the accompanying game names (separated by comma's if there are more)
@@ -447,6 +532,139 @@ async def add_games(ctx, *, game_list=None):
         else:
             await channel.send(style(
                 f'Done! I added {len(to_add)} new games, as {len(already_added)} were already added.'))
+
+
+# The get error command
+@disco.command("wazzup")
+@status_update
+async def get_error(ctx, forget=None):
+    """ [forget] Show or forget the current status of the Disco bot
+
+    Lists potential caught exceptions in this server. Can also be used to clear all stored caught exceptions
+    (!wazzup forget)
+    """
+
+    # Get guild name
+    guild_name = guild_sql_table(ctx.message.guild.name)
+
+    # See if there are any exceptions raised since last get_error request
+    excs = []
+    if guild_name in exceptions.keys():
+        if len(exceptions[guild_name]):
+            excs = exceptions[guild_name]
+
+    # See if we need to clear all errors
+    if forget is not None and forget.lower() == "forget":
+        # Create message based on if there were exceptions
+        mssg = f"I'm good, nothing to forget! {em_smile}"
+        if len(excs) >= 1:
+            issue_str = "issue" if len(excs) == 1 else "issues"
+            mssg = f"Few... I just forgot about {len(excs)} {issue_str} in the world!\n{em_put_table}"
+
+        # Clear all errors/exceptions
+        exceptions[guild_name] = []
+
+        # Send message
+        await ctx.channel.send(mssg)
+
+        return
+
+    # If none were found, send that the bot is all good and return
+    if len(excs) == 0:
+        mssg = f"I'm all good! {em_smile}"
+        await ctx.message.channel.send(mssg)
+        return
+
+    # Else, format the errors and send them
+    mssg = f"Since you ask... I did run into some issues!\n{em_throw_table}"
+    await ctx.message.channel.send(mssg)
+    mssg = "The following exceptions were caught:"
+    for exc in excs:
+        # Get some exception data
+        err = exc["error"]
+        ctx = exc["context"]
+        err_name = str(err).split(": ")
+        if len(err_name) > 1:
+            err_name = " ".join(err_name[1:])
+        author = ctx.author.name
+        cmd = ctx.message.content
+        time = ctx.message.created_at.strftime("%m/%d/%Y, %H:%M:%S")
+
+        # Format message to send
+        mssg += f"\n- {err_name} was raised when {author} used {cmd} at {time}"
+
+    await ctx.message.channel.send(style(mssg))
+
+
+# Get a list of people who play a certain game
+@disco.command("whoplays")
+@status_update
+async def get_members(ctx, *, game):
+    """ <game>  Lists the server members who registered a game.
+
+    It lists all of the people in this server who registered the given game (e.g. !whoplays Minecraft). Only one game
+    can be queried at a time, though it is not case sensitive.
+    """
+
+    # Get the channel
+    channel = ctx.message.channel
+
+    # Format the game name
+    game = game.title()
+
+    # Get the guild data
+    guild_name = ctx.guild.name
+    guild_df = pd.read_sql_query(f"SELECT * from {guild_sql_table(guild_name)}", sql_connection)
+
+    # Get all of the member ID's who registered that game
+    ids = guild_df[guild_df[game_col] == game]
+    names = list(ids[user_name_col].values)
+
+    # Get the message's author
+    author = ctx.message.author.name
+
+    # Create opening if no one registered the game,
+    if len(names) == 0:
+        # send message and return
+        mssg = f"Sadly none have registered {game} to me {em_sad}"
+        await channel.send(mssg)
+        return
+
+    # only the author registered it
+    elif len(names) == 1 and names[0] == author:
+        # send message and return
+        mssg = f"It seems only you have registered {game} to me."
+        await channel.send(mssg)
+        return
+
+    # Others have this game registered
+    else:
+
+        if author in names:
+            # Remove self from the names
+            names.remove(author)
+
+            # Create name list message
+            names_mssg = "The following people have this game: " + ", ".join(names) + "\n"
+
+            # Create message
+            others_str = "others" if len(names) > 1 else "other"
+            mssg = f"{len(names)} {others_str} also registered {game} to me beside you!\n"
+            mssg += names_mssg
+
+            # Send messages
+            await channel.send(mssg)
+            time.sleep(0.2)
+            await channel.send(f".\n{em_play_game.replace('game', game)}")
+
+        else:
+            # Create message
+            others_str = "others" if len(names) > 1 else "other"
+            mssg = f"{len(names)} {others_str} have registered {game} to me! Do you also play this game? If so you" \
+                   f" can register it to me with !add {game}."
+
+            # Send message
+            await channel.send(mssg)
 
 
 def run_bot(test_mode=False, reset_databases=False):
